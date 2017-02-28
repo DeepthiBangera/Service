f3886612f55980e34ab7f9d3cc7e1d97060e092e
DeepthiBangera <deepthibangera13@gmail.com>
2017-02-28 14:55:18 +0530

    Add files via upload

---
+++ WindowsService_Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.IO;
+
+namespace WindowsService_Test
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun;
+            if (Environment.UserInteractive)
+            {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+            }
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service1() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
